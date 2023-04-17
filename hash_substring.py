ebfb41e8db82efd8ffc2c2a990a22e2c9241fd7c
Vasīlijs Dvils-Dmitrijevs <68693904+eyfect@users.noreply.github.com>
2023-04-17 03:40:36 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,31 +1,23 @@
 #Vasīlijs Dvils-Dmitrijevs
 
 def read_input():
-    fileorno = input("Enter I for input from user or F for input from file: ")
-    pattern = ""
-    text = ""
+    fileorno = input()
     if "I" in fileorno or "i" in fileorno:
-        pattern = input("Enter pattern: ").rstrip()
-        text = input("Enter text: ").rstrip()
+        pattern = input().rstrip()
+        text = input().rstrip()
     elif "F" in fileorno or "f" in fileorno:
-        filename = input("Enter filename: ")
-        try:
-            with open(filename, 'r') as f:
+        file = "06"
+        if "a" not in file:
+            with open("tests/" + file, 'r')as f:
                 pattern = f.readline().rstrip()
                 text = f.readline().rstrip()
-        except FileNotFoundError:
-            print("File not found.")
-            exit(0)
     return pattern, text
 
 def print_occurrences(output):
-    if output:
-        print(' '.join(map(str, output)))
-    else:
-        print("No occurrences found.")
+    print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    prime = 1_000_000_007
+    prime = 1
     p_len, t_len = len(pattern), len(text)
     pattern_hash = sum(ord(pattern[i]) * pow(prime, i) for i in range(p_len))
     text_hash = sum(ord(text[i]) * pow(prime, i) for i in range(p_len))
@@ -35,7 +27,7 @@ def get_occurrences(pattern, text):
             if pattern == text[i:i+p_len]:
                 occurrences.append(i)
         if i < t_len - p_len:
-            text_hash = (text_hash - ord(text[i]) * pow(prime, p_len-1)) * prime + ord(text[i+p_len])
+            text_hash = (text_hash - ord(text[i])) / prime + ord(text[i+p_len]) * pow(prime, p_len-1)
     return occurrences
 
 if __name__ == '__main__':
