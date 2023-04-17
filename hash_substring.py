0a79d346b831704b8b904c832d4c2db06176cffa
Vasīlijs Dvils-Dmitrijevs <68693904+eyfect@users.noreply.github.com>
2023-04-17 03:20:25 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,46 +1,30 @@
 #Vasīlijs Dvils-Dmitrijevs
 
-from sys import stdin
-
 def read_input():
-file = None
-test_file = "./tests/06"
+    input_choice = input().upper()
+    if "I" in input_choice:
+        return input().rstrip(), input().rstrip()
+    elif "F" in input_choice:
+        filename = input().rstrip()
+        with open(filename) as f:
+            return f.readline().rstrip(), f.readline().rstrip()
 
-mode = input()
-if "I" in mode:
-    file = stdin
-
-elif "F" in mode:
-    file = open(test_file, encoding="UTF-8")
-
-else:
-    exit()
-
-pattern = file.readline().rstrip()
-text = file.readline().rstrip()
-
-return (pattern, text)
 def print_occurrences(output):
-print(' '.join(map(str, output)))
+    print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-prime = 17
-bucket = 256
-def hasher(string):
-nonlocal prime, bucket
-result = 0
-for char in string:
-result = (prime * result + ord(char)) % bucket
-return result
-t_length = len(text)
-p_length = len(pattern)
-p_hash = hasher(pattern)
+    occurrences = []
+    if len(pattern) > len(text):
+        return occurrences
+    hashP = hash(pattern)
+    hashT = hash(text[:len(pattern)])
+    for i in range(len(text)-len(pattern)+1):
+        if hashP == hashT:
+            if text[i:i+len(pattern)] == pattern:
+                occurrences.append(i)
+        if i < len(text) - len(pattern):
+            hashT = hash(text[i+1:i+len(pattern)+1])
+    return occurrences
 
-window = None
-for i in range(t_length - p_length + 1):
-    window = text[i:i + p_length]
-    if p_hash == hasher(window):
-        if pattern == window:
-            yield i
-            if name == 'main':
-print_occurrences(get_occurrences(*read_input()))
+if __name__ == '__main__':
+    print_occurrences(get_occurrences(*read_input()))
