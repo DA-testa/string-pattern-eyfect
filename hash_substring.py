1e4eaeefd3f248d1e50a56cee58011ff6253a611
Vasīlijs Dvils-Dmitrijevs <68693904+eyfect@users.noreply.github.com>
2023-04-17 03:02:01 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,35 +1,43 @@
-# Vasīlijs Dvils-Dmitrijevs
+#Vasīlijs Dvils-Dmitrijevs
 
 def read_input():
+   
     choice = input()
-
-    if 'I'in(choice):
+    if 'I' in choice:
+  
         return (input().rstrip(), input().rstrip())
     else:
-        with open('tests/06', "r") as fails:
-            return (fails.readline().rstrip(),fails.readline().rstrip())
+        with open('tests/06', 'r') as file:
+            return (file.readline().rstrip(), file.readline().rstrip())
 
 def print_occurrences(output):
+    
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    listik = []
-    mejik_namber = 7
-    mejik_mef = lambda s: sum(ord(c) for c in s)
     
-    pattern_hash = mejik_mef(pattern) * mejik_namber
+    occurrences = []
+    prime = 10 ** 9 + 7
+    base = 131
+    pattern_len = len(pattern)
+    text_len = len(text)
+
     
-    for i in range(len(text) - len(pattern) + 1):
-        substring = text[i:i+len(pattern)]
-        substring_hash = mejik_mef(substring) * mejik_namber
-        
-        if substring_hash == pattern_hash:
-            if substring == pattern:
-                listik.append(i)
+    pattern_hash = sum(ord(c) * pow(base, i, prime) for i, c in enumerate(pattern)) % prime
+    window_hash = sum(ord(c) * pow(base, i, prime) for i, c in enumerate(text[:pattern_len])) % prime
+
     
-    return listik
+    for i in range(text_len - pattern_len + 1):
+        if pattern_hash == window_hash:
+            
+            if pattern == text[i:i+pattern_len]:
+                occurrences.append(i)
+        if i < text_len - pattern_len:
+            
+            window_hash = (window_hash - ord(text[i]) * pow(base, pattern_len-1, prime)) % prime
+            window_hash = (window_hash * base + ord(text[i+pattern_len])) % prime
+    return occurrences
+
 
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-
-
