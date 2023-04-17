ede866ff024e81837f829f80bd58a44991ca4c21
Vasīlijs Dvils-Dmitrijevs <68693904+eyfect@users.noreply.github.com>
2023-04-17 03:23:05 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -5,26 +5,24 @@ def read_input():
     if "I" in input_choice:
         return input().rstrip(), input().rstrip()
     elif "F" in input_choice:
-        filename = input().rstrip()
-        with open(filename) as f:
-            return f.readline().rstrip(), f.readline().rstrip()
-
+        with open("tests/06") as x:
+            result= x.readline().rstrip(),x.readline().rstrip()
+            return result
 def print_occurrences(output):
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    occurrences = []
+    oc = []
     if len(pattern) > len(text):
-        return occurrences
+        return oc
     hashP = hash(pattern)
     hashT = hash(text[:len(pattern)])
     for i in range(len(text)-len(pattern)+1):
         if hashP == hashT:
             if text[i:i+len(pattern)] == pattern:
-                occurrences.append(i)
+                oc.append(i)
         if i < len(text) - len(pattern):
             hashT = hash(text[i+1:i+len(pattern)+1])
-    return occurrences
-
+    return oc
 if __name__ == '__main__':
-    print_occurrences(get_occurrences(*read_input()))
+    print_oc(get_oc(*read_input()))
