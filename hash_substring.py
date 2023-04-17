1301fd0d7fa05fa993c86fb030a9c52d8e4286ba
Vasīlijs Dvils-Dmitrijevs <68693904+eyfect@users.noreply.github.com>
2023-04-17 03:25:19 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -2,27 +2,37 @@
 
 def read_input():
     input_choice = input().upper()
-    if "I" in input_choice:
-        return input().rstrip(), input().rstrip()
-    elif "F" in input_choice:
-        with open("tests/06") as x:
-            result= x.readline().rstrip(),x.readline().rstrip()
-            return result
+    if input_choice == "I":
+        pattern = input().rstrip()
+        text = input().rstrip()
+    elif input_choice == "F":
+        filename = input().rstrip()
+        with open(filename) as f:
+            pattern = f.readline().rstrip()
+            text = f.readline().rstrip()
+    return pattern, text
+
 def print_occurrences(output):
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    oc = []
+    occurrences = []
     if len(pattern) > len(text):
-        return oc
-    hashP = hash(pattern)
-    hashT = hash(text[:len(pattern)])
-    for i in range(len(text)-len(pattern)+1):
-        if hashP == hashT:
+        return occurrences
+    prime = 1000000007
+    x = 263
+    p_hash = sum(ord(pattern[i]) * pow(x, i, prime) for i in range(len(pattern))) % prime
+    t_hash = sum(ord(text[i]) * pow(x, i, prime) for i in range(len(pattern))) % prime
+    x_to_len_pattern = pow(x, len(pattern), prime)
+    for i in range(len(text) - len(pattern) + 1):
+        if p_hash == t_hash:
             if text[i:i+len(pattern)] == pattern:
-                oc.append(i)
+                occurrences.append(i)
         if i < len(text) - len(pattern):
-            hashT = hash(text[i+1:i+len(pattern)+1])
-    return oc
+            t_hash = (t_hash - ord(text[i]) * pow(x, len(pattern)-1, prime)) % prime
+            t_hash = (t_hash * x + ord(text[i+len(pattern)])) % prime
+            t_hash = (t_hash + prime) % prime
+    return occurrences
+
 if __name__ == '__main__':
-    print_oc(get_oc(*read_input()))
+    print_occurrences(get_occurrences(*read_input()))
